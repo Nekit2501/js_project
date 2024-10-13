fcdab715cc6527937272ba1785336b473054bf43
Nekit2501 <131582700+Nekit2501@users.noreply.github.com>
2024-10-13 12:15:58 +0300

    1

---
+++ index.js
@@ -0,0 +1,7 @@
+const n = document.querySelector(".n");
+const k = document.querySelector(".k");
+const p = document.querySelector(".p");
+const result = document.querySelector(".result");
+const WinLos = document.querySelector(".winLos");
+n.addEventListener("click", Scissors);
+function Scissors() {}

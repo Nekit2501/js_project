d620c95d6da462a7419830ff889fe9d513ba2444
Nekit2501 <131582700+Nekit2501@users.noreply.github.com>
2024-10-20 10:51:57 +0300

    ok

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ const k = document.querySelector(".k");
 const p = document.querySelector(".p");
 const result = document.querySelector(".result");
 const WinLos = document.querySelector(".winLos");
+
+let user = 0;
+let comp = 0;
+let comp = Math.random();
+
 n.addEventListener("click", Scissors);
+k.addEventListener("click", Rock);
+p.addEventListener("click", Paper);
+
 function Scissors() {}
+
+function Rock() {}
+
+function Paper() {}

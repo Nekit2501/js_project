e4acc94faf3e931e15b40ad63b42d00df9b105d0
Nekit2501 <131582700+Nekit2501@users.noreply.github.com>
2024-10-27 11:44:27 +0300

    камень ножницы

--- index.js
+++ index.js
@@ -2,18 +2,71 @@ const n = document.querySelector(".n");
 const k = document.querySelector(".k");
 const p = document.querySelector(".p");
 const result = document.querySelector(".result");
-const WinLos = document.querySelector(".winLos");
+const Win = document.querySelector(".Win");
+const Los = document.querySelector(".Los");
+const not = document.querySelector(".not");
 
+const compA = ["scissors", "rock", "paper"];
+
+let nothing = 0;
 let user = 0;
 let comp = 0;
-let comp = Math.random();
 
 n.addEventListener("click", Scissors);
 k.addEventListener("click", Rock);
 p.addEventListener("click", Paper);
 
-function Scissors() {}
+function Scissors() {
+  const user1 = "scissors";
+  const compLet = Lets();
+  const scissorsR = user > compA;
+  console.log(compLet);
+  if (user1 === compLet) {
+    nothing = nothing + 1;
+    not.textContent = nothing;
+  } else if (compLet === "paper") {
+    user = user + 1;
+    Win.textContent = user;
+  } else {
+    comp = comp + 1;
+    Los.textContent = comp;
+  }
+}
 
-function Rock() {}
+function Rock() {
+  const user1 = "rock";
+  const compLet = Lets();
+  console.log(compLet);
+  if (user1 === compLet) {
+    nothing = nothing + 1;
+    not.textContent = nothing;
+  } else if (compLet === "scissors") {
+    user = user + 1;
+    Win.textContent = user;
+  } else {
+    comp = comp + 1;
+    Los.textContent = comp;
+  }
+}
 
-function Paper() {}
+function Paper() {
+  const user1 = "paper";
+  const compLet = Lets();
+  if (user1 === compLet) {
+    nothing = nothing + 1;
+    not.textContent = nothing;
+  } else if (compLet === "rock") {
+    user = user + 1;
+    Win.textContent = user;
+  } else {
+    comp = comp + 1;
+    Los.textContent = comp;
+  }
+}
+function Lets() {
+  let compLet = compA[Math.floor(Math.random() * compA.length)];
+  result.textContent = compLet;
+  return compLet;
+}
+
+console.log();
